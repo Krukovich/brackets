def670c248766a4b362b3ca33bc655ce52f907ae
Alexey <44588100+Krukovich@users.noreply.github.com>
2020-03-07 12:15:39 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    const confArray = bracketsConfig.map(item => item.join(""));
+    let j = 0;
+
+    while (j <= bracketsConfig.length) {
+        for(let i = 0; i < confArray.length; i++) {
+            if (str.includes(confArray[i], 0)) {
+                str = str.replace(confArray[i], "");
+                i--;
+            }
+        }
+        j++;
+    }
+    return (str.length === 0) ? true : false;
 }
+
+
